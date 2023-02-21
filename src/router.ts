c951eb31784897e55293dd446e8aba41e920c4ed
mrochny <tpalinski@icloud.com>
2023-02-21 13:33:46 +0100

    create employees router

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import Express, { NextFunction, Request, Response } from "express";
+import { employeeRouter } from "./routes/employees";
 
 export let router = Express();
+
+router.use('/employees', employeeRouter);
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.status(404).send('Please enter valid API path')
