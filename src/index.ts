6cd4ea04f96065df00334138d4018d1e14b717b9
mrochny <tpalinski@icloud.com>
2023-02-21 12:21:45 +0100

    basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log("Hello");
-console.log("World");
+import Express, {Response, Request, NextFunction} from "express";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./db";
+
+
+const app = Express();
+dotenv.config();
+connectToDatabase();
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send("Hello");
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is runinng on port ${PORT}`)
+})
